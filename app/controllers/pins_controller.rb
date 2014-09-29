a29f55d8e167b9a34a1e34f02963b1d18ddecbad
stbarrientos <stbarrientos@gmail.com>
2014-09-29 10:49:46 -0400

    db + mvc initial configuration complete

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,9 @@
+class PinsController < ApplicationController
+
+	def index
+	end
+
+	def create
+	end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Pin.delete_all
+
+Pin.create([
+ {lat: 38.0081, long: 77.0997},
+ {lat: 40.7127, long: 74.0059},
+ {lat: 37.7833, long: 122.4167}
+])
